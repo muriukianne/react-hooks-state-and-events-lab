1392f9947031fa7b6817bf1c91044887280f4dae
Anne Muriuki <muriukianne05@gmail.com>
2024-11-14 14:43:47 +0300

    add state and event lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [addItem, setAddItem]= useState(false);
+
   return (
-    <li className="">
+    <li className={addItem ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={()=> setAddItem(!addItem)}>{addItem ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
